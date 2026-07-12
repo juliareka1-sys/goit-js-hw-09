2413e0144cd5afd96abe255d253c02a79c6642dc
JR <julia.reka1@gmail.com>
2026-07-12 21:43:54 +0200

    task2 fixed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,19 +7,20 @@ const formData = {
 
 
 const form = document.querySelector(".feedback-form");
+fillFromStorage();
 
 form.addEventListener("input", inputHandler);
 form.addEventListener("submit", submitHandler);
 
 function inputHandler(event) {
     const key = event.target.name;
-    const userData = event.target.value.trim();
+    const userData = event.target.value;
     formData[key] = userData;
     localStorage.setItem(storageKey,JSON.stringify(formData));
     console.log(formData);
 };
 
-fillFromStorage();
+
 function fillFromStorage (){
     const isData = localStorage.getItem(storageKey);
     
