a5b8fab464367d9958c3cb97d67ae416dd7d8699
JR <julia.reka1@gmail.com>
2026-07-12 21:16:19 +0200

    task2

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+const storageKey = "feedback-form-state";
+
+const formData = {
+    email: "",
+    message: ""
+};
+
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", inputHandler);
+form.addEventListener("submit", submitHandler);
+
+function inputHandler(event) {
+    const key = event.target.name;
+    const userData = event.target.value.trim();
+    formData[key] = userData;
+    localStorage.setItem(storageKey,JSON.stringify(formData));
+    console.log(formData);
+};
+
+fillFromStorage();
+function fillFromStorage (){
+    const isData = localStorage.getItem(storageKey);
+    
+    if (isData) {
+        const savedData = JSON.parse(isData);
+        form.elements.email.value = savedData.email;
+        form.elements.message.value = savedData.message;
+
+        formData.email = savedData.email;
+        formData.message = savedData.message;
+
+    }
+};
+    
+function submitHandler(event) {
+    event.preventDefault();
+    if (formData.email === "" || formData.message === "") {
+        alert('Fill please all fields');
+        return;
+  }
+    console.log(formData);
+    localStorage.removeItem(storageKey);
+    form.reset();
+};
+
+
